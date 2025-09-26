efb5d356f2c862b63711d83d0b542adaeec0eb7d
Klein <bibeksharma976@gmail.com>
2025-09-27 00:39:37 +0545

    added password reset function-still missing button though

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,6 +135,14 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGOUT_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bibehsharma777@gmail.com'
+EMAIL_HOST_PASSWORD = 'jxllubosbvycclbq'
+DEFAULT_FROM_EMAIL = 'bibehsharma777@gmail.com'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
