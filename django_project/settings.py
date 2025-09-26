669a4f6d34f4be853bfb717dd73e3cf0f098aef5
Klein <bibeksharma976@gmail.com>
2025-09-27 00:53:56 +0545

    adding password reset function without button

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,9 +139,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bibehsharma777@gmail.com'
-EMAIL_HOST_PASSWORD = 'jxllubosbvycclbq'
-DEFAULT_FROM_EMAIL = 'bibehsharma777@gmail.com'
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('DB_MAIL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
